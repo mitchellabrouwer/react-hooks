19b2250c972d25bdcbc7f3e1b7e43ec355f048e8
Mitch <mbarron198@gmail.com>
2020-10-16 21:53:34 +1100

    greeting with useState

---
+++ 01.js
@@ -0,0 +1,50 @@
+// useState: greeting
+// http://localhost:3000/isolated/exercise/01.js
+
+import React, { useState } from "react";
+
+// Extra 1 greeting with initial prop
+
+function Greeting({ initialName = "" }) {
+  const [name, setName] = useState(initialName);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : "Please type your name"}
+    </div>
+  );
+}
+
+function App() {
+  return <Greeting initialName="Mitch" />;
+}
+
+export default App;
+
+// Exercise
+
+// function Greeting() {
+//   const [name, setName] = useState('')
+
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
