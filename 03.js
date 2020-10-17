a182f8415e2d4f497bc93e2a9a52651587e4e61a
Mitch <mbarron198@gmail.com>
2020-10-17 22:24:12 +1100

    lifting state completed

---
+++ 03.js
@@ -0,0 +1,86 @@
+// Lifting state
+// http://localhost:3000/isolated/exercise/03.js
+
+import React from "react";
+
+function Name() {
+  const [name, setName] = React.useState("");
+  return (
+    <div>
+      <label htmlFor="name">Name: </label>
+      <input
+        id="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
+    </div>
+  );
+}
+
+function FavoriteAnimal({ animal, onAnimalChange }) {
+  return (
+    <div>
+      <label htmlFor="animal">Favorite Animal: </label>
+      <input id="animal" value={animal} onChange={onAnimalChange} />
+    </div>
+  );
+}
+
+function Display({ animal }) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>;
+}
+
+function App() {
+  // const [animal, setAnimal] = React.useState("");
+  return (
+    <form>
+      <Name />
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={(event) => setAnimal(event.target.value)}
+      />
+      <Display animal={animal} />
+    </form>
+  );
+}
+
+export default App;
+
+// Exercise
+
+// function Name({name, onNameChange}) {
+//   return (
+//     <div>
+//       <label htmlFor="name">Name: </label>
+//       <input id="name" value={name} onChange={onNameChange} />
+//     </div>
+//   )
+// }
+
+// function FavoriteAnimal({animal, onAnimalChange}) {
+//   return (
+//     <div>
+//       <label htmlFor="animal">Favorite Animal: </label>
+//       <input id="animal" value={animal} onChange={onAnimalChange} />
+//     </div>
+//   )
+// }
+
+// function Display({name, animal}) {
+//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+// }
+
+// function App() {
+//   const [animal, setAnimal] = React.useState('')
+//   const [name, setName] = React.useState('')
+//   return (
+//     <form>
+//       <Name name={name} onNameChange={event => setName(event.target.value)} />
+//       <FavoriteAnimal
+//         animal={animal}
+//         onAnimalChange={event => setAnimal(event.target.value)}
+//       />
+//       <Display name={name} animal={animal} />
+//     </form>
+//   )
+// }
